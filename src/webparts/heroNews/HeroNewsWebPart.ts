4ad2a31b9ed220fc01e232c919e1aac834229de3
Martin <Robert.Martin@insight.com>
2020-10-27 10:12:29 -0400

    init commit

---
+++ src/webparts/heroNews/HeroNewsWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'HeroNewsWebPartStrings';
+import HeroNews from './components/HeroNews';
+import { IHeroNewsProps } from './components/IHeroNewsProps';
+
+export interface IHeroNewsWebPartProps {
+  description: string;
+}
+
+export default class HeroNewsWebPart extends BaseClientSideWebPart<IHeroNewsWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IHeroNewsProps> = React.createElement(
+      HeroNews,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/heroNews/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHeroNewsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HeroNewsWebPartStrings' {
+  const strings: IHeroNewsWebPartStrings;
+  export = strings;
+}
